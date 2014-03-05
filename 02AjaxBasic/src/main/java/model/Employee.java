da888981695c0104f6cb211c829ed97e671d130b
Tom <tompels@gmail.com>
2014-03-05 20:19:54 +0100

    - added employee

---
+++ 02AjaxBasic/src/main/java/model/Employee.java
@@ -0,0 +1,69 @@
+package model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by u0090265 on 3/5/14.
+ */
+
+@Entity
+@Table(name = "medewerker")
+public class Employee {
+    private long id;
+    private String name;
+    private String email;
+    private Address Address;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @NotNull
+    @Column(name="naam")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @NotNull
+    @OneToOne(cascade=CascadeType.ALL,fetch=FetchType.EAGER)
+    @JoinColumn(name="adres",insertable=true, updatable=true, nullable=false,unique=true)
+    public Address getAddress() {
+        return Address;
+    }
+
+    public void setAddress(Address address) {
+        this.Address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return  new StringBuilder("Id: ").append(getId())
+                .append(" name: ").append(getName())
+                .append(" email: ").append(getEmail())
+                .append(" address: ").append(getAddress()).toString();
+    }
+}
+
+
